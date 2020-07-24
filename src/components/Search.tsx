543334d2908c5c3b03e40c392992659b29b9d0ba
SavostinIllia <savostin.illia@gmail.com>
2020-07-24 12:22:41 +0200

    creating github reducer for search request

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AlertContext } from "../context/alert/alertContext";
 
 interface SearchProps {}
 
 const Search: React.FC<SearchProps> = ({}) => {
+  const [value, setValue] = useState("");
+
   const { show } = useContext(AlertContext);
   const onsubmit = (e: any) => {
-    if (e.key === "Enter") {
-      show("This is alert");
+    if (e.key !== "Enter") {
+      return;
+    }
+    if (value.trim()) {
+      console.log(value);
+    } else {
+      show("Enter the user data");
     }
   };
   return (
@@ -16,6 +23,7 @@ const Search: React.FC<SearchProps> = ({}) => {
         type="text"
         placeholder="Type nickname..."
         className="form-control"
+        onChange={(e) => setValue(e.target.value)}
         onKeyPress={onsubmit}
       />
     </div>
